ba88aec1592fe4053c88142aaa3e13a872005780
Imran <razaviimran@gmail.com>
2021-08-18 15:14:41 +0530

    updated

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ today = datetime.now()
 today_tuple = (today.month, today.day)
 
 data = pandas.read_csv("birthdays.csv")
-birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+birthday_dict = {
+    (data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()
+}
 
 if today_tuple in birthday_dict:
     birthday_person = birthday_dict[today_tuple]
